4dc14e4da33b2439a346bd01f41218d8d91b1c0f
Aung Ko Man <aungkoman@gmail.com>
2024-01-29 13:10:56 +0630

    login authentication logic added

--- app/urls.py
+++ app/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login/", views.login, name="login"),
-    path("register/", views.register, name="register"),
+    path("login/", views.login_page, name="login"),
+    path("register/", views.register_page, name="register"),
     path("login-check/", views.login_check, name="login_check"),
     path("register-check/", views.register_check, name="register_check"),
     path("dasbhoard/", views.dasbhoard, name="dasbhoard"),
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.contrib.auth import login, authenticate #add this
 
 def helloWorld(request):
     return HttpResponse("Hello, world. You're at the money app -> index method")
@@ -10,18 +10,28 @@ def index(request):
     person_list = ['Aung Aung', 'Ma Ma', 'Mg Mg']
     return render(request, 'index.html',{'person_list': person_list, 'title' : 'Person List' })
 
-def login(request):
+def login_page(request):
     #return HttpResponse("Hello, world. You're at the money app -> index method")
     person_list = ['Aung Aung', 'Ma Ma', 'Mg Mg']
     return render(request, 'login.html',{'person_list': person_list, 'title' : 'Person List' })
 
-def register(request):
+def register_page(request):
     person_list = ['Aung Aung', 'Ma Ma', 'Mg Mg']
     return render(request, 'register.html',{'person_list': person_list, 'title' : 'Person List' })
 
 def login_check(request):
-    person_list = ['Aung Aung', 'Ma Ma', 'Mg Mg']
-    return render(request, 'dashboard.html',{'person_list': person_list, 'title' : 'Person List' })
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username= username, password=password)
+    if user is not None:
+        # A backend authenticated the credentials
+        login(request, user)
+        return redirect("user_panel")
+    else:
+        # No backend authenticated the credentials
+        return HttpResponse("credential does not match " + username + ", " + password) #render(request, 'user_management/login.html')
+    # person_list = ['Aung Aung', 'Ma Ma', 'Mg Mg']
+    # return render(request, 'dashboard.html',{'person_list': person_list, 'title' : 'Person List' })
 
 def register_check(request):
     person_list = ['Aung Aung', 'Ma Ma', 'Mg Mg']
