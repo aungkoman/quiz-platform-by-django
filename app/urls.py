bfd585864850d150de72f7ebf2669bec527579aa
Aung Ko Man <aungkoman@gmail.com>
2024-01-30 15:44:41 +0630

    answer URLs added

--- app/urls.py
+++ app/urls.py
@@ -28,4 +28,13 @@ urlpatterns = [
     path('question/store/', views.question_store, name='question_store'),
     path('question/update/<int:question_id>/', views.question_update, name='question_update'),
     path('question/delete/<int:question_id>/', views.question_delete, name='question_delete'),
+
+    # Answer - UI
+    path('answer/create/', views.answer_create_page, name='answer_create_page'),
+    path('answer/detail/<int:answer_id>/', views.answer_detail_page, name='answer_detail_page'),
+    path('answer/edit/<int:answer_id>/', views.answer_edit_page, name='answer_edit_page'),
+    # Answer - Business Logic
+    path('answer/store/', views.answer_store, name='answer_store'),
+    path('answer/update/<int:answer_id>/', views.answer_update, name='answer_update'),
+    path('answer/delete/<int:answer_id>/', views.answer_delete, name='answer_delete'),
 ]
